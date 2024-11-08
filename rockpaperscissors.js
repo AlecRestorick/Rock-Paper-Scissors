4a6b68803b72dd0bad2aec366b3250b0a3145e57
AlecRestorick <185495790+AlecRestorick@users.noreply.github.com>
2024-11-08 11:01:47 +0200

    Added restart function and edited alert messages.HumanChoice,ComputerChoice,PlayRound,PlayGame and RestartGame = Full 5 Round Game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -18,7 +18,9 @@ const validChoices = ['rock', 'paper', 'scissors'];
 	do{
 		humanSelect = prompt(`Rock,Paper or Scissors?`).toLowerCase();
 		if (!validChoices.includes(humanSelect)) {
-		alert(`${humanSelect} is Invalid! Please select Rock, Paper, or Scissors.`);
+		alert(`
+			${humanSelect} is Invalid! 
+			Please select Rock, Paper, or Scissors.`);
 		}
 	}while (!validChoices.includes(humanSelect));
 return humanSelect;
@@ -29,7 +31,8 @@ return humanSelect;
 function playRound(humanChoice, computerChoice) {
 	if(humanChoice===computerChoice){
 		round++
-		alert(`Draw! 
+		alert(`
+			Draw! 
 			${humanChoice.charAt(0).toUpperCase()+humanChoice.slice(1)} = ${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)} 
 			Player Score: ${humanScore} 
 			Computer Score: ${computerScore} 
@@ -41,7 +44,8 @@ function playRound(humanChoice, computerChoice) {
 		){
 		humanScore++;
 		round++
-		alert(`You Win! 
+		alert(`
+			You Win! 
 			${humanChoice.charAt(0).toUpperCase()+humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}! 
 			Player Score: ${humanScore} 
 			Computer Score: ${computerScore} 
@@ -49,7 +53,8 @@ function playRound(humanChoice, computerChoice) {
 	}else{
 		computerScore++;
 		round ++
-		alert(`You Lose! 
+		alert(`
+			You Lose! 
 			${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase()+humanChoice.slice(1)}! 
 			Player Score: ${humanScore} 
 			Computer Score: ${computerScore} 
@@ -59,23 +64,49 @@ function playRound(humanChoice, computerChoice) {
 
 //playGame function
 function playGame(){
+
+	round=0;
+	humanScore = 0;
+	computerScore = 0;
+
+
 	while(round<5){
 		const computerSelection = getComputerChoice();
 		const humanSelection = getHumanChoice();
 		playRound(humanSelection,computerSelection);
 }
 if(humanScore>computerScore){
-	alert(`Game Over! You Win the Game!
-		Final Score: Player Score: ${humanScore} - Computer Score: ${computerScore}`)
+	alert(`
+		Game Over! 
+		You Win the Game!
+		Final Score: Player: ${humanScore} - Computer: ${computerScore}`)
 }else if(computerScore>humanScore){
-	alert(`Game Over! You Lose the Game!
-		Final Score: Player Score: ${humanScore} - Computer Score: ${computerScore}`)
+	alert(`
+		Game Over! 
+		You Lose the Game!
+		Final Score: Player: ${humanScore} - Computer: ${computerScore}`)
 }else{
-	alert(`Game Over! It's a Draw!
-		Final Score: Player Score: ${humanScore} - Computer Score: ${computerScore}`)
+	alert(`
+		Game Over! 
+		It's a Draw!
+		Final Score: Player: ${humanScore} - Computer: ${computerScore}`)
 }
 }
-playGame()
+
+
+
+function restartGame() {
+    let playAgain = confirm("Do you want to play again?");
+    if (playAgain) {
+        playGame();
+    } else {
+        alert("Thanks for playing!");
+    }
+}
+
+do {
+    playGame();
+} while (confirm("Do you want to play again?"));
 
 
 
